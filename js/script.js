7eca9deb4d6f4a1646488430a5a1dd0137594996
ksb <jwe06120@gmail.com>
2024-06-07 18:09:41 +0900

    슬라이드 modal

--- js/script.js
+++ js/script.js
@@ -73,4 +73,30 @@ $(function () {
       $aniEl.removeClass('aos-animate');
     },
   });
+
+  // 모달 열기
+  function openModal(imageSrc) {
+    console.log(imageSrc);
+    $('#modal-img').attr('src', imageSrc);
+    $('#modal').css('display', 'block');
+  }
+
+  // 모달 닫기
+  function closeModal() {
+    $('#modal').css('display', 'none');
+  }
+
+  // 모달 닫기 버튼 클릭 이벤트 처리
+  $('#close').on('click', function () {
+    closeModal();
+  });
+
+  // 각 버블 요소에 클릭 이벤트 추가
+  $('.bubble').on('click', function () {
+    var activeIndex = $('.fp-slidesNav.bottom ul li a.active').parent().index();
+    // 활성화된 슬라이드의 이미지 src를 가져와서 출력
+    var src = $('.main-slider .slide').eq(activeIndex).find('.project-img img').attr('src');
+    console.log(src);
+    openModal(src);
+  });
 });
